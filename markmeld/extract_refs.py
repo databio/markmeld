2ca793c2f868d3ade313b0b58e7dd50e286a8822
nsheff <nsheff@users.noreply.github.com>
2023-05-18 09:22:54 -0400

    some docs clarification

--- markmeld/extract_refs.py
+++ markmeld/extract_refs.py
@@ -1,15 +1,16 @@
 import re
 
+def extract_refs(value: str) -> list:
+    """
+    This is a custom jinja filter that will find and return pandoc-citeproc-style refs, like
+    "@ReferenceKey", from a given string.
 
-# This function will extract pandoc-citeproc-style refs, like
-# @ReferenceKey, from a given value.
-def extract_refs(value):
-    # :param: value Str The string you wish to extract references from
-    # :return: list List of the reference keys (with @ removed).
+    :param: value Str The string you wish to extract references from
+    :return: list List of the reference keys (with @ removed).
+    """
+    # Example:
+    # m = extract_refs("abc; hello @test;me @second one; and finally @three")
+    # m  # returns ['test', 'second', 'three']
+
     m = re.finditer("@([a-zA-Z0-9_])*", value)
     return [x.group() for x in m]
-
-
-# Example:
-# m = extract_refs("abc; hello @test;me @second one; and finally @three")
-# m
