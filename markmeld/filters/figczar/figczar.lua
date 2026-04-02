2df385ca2a342309d5b3a7564de6bead7595a2c9
nsheff <nsheff@users.noreply.github.com>
2026-04-02 13:38:20 -0400

    Fix figczar for pandoc 3 Figure AST element
    Pandoc 3 wraps images in a Figure element, causing figure* to nest
    inside figure (illegal LaTeX). Rewrite figczar with two-pass
    architecture: pass 1 handles Figure elements (pandoc 3), pass 2
    handles bare Image elements (pandoc 2 fallback).

--- markmeld/filters/figczar/figczar.lua
+++ markmeld/filters/figczar/figczar.lua
@@ -1,8 +1,3 @@
--- function Strong(elem)
---   return pandoc.SmallCaps(elem.c)
--- end
-
-
 -- debugging function. Use it to dump a nested dict
 function dump(o)
    if type(o) == 'table' then
@@ -17,60 +12,85 @@ function dump(o)
    end
 end
 
-
--- use by adding {wrap=3} to the image
 if FORMAT:match 'latex' then
-  function Image(elem)
-    -- wrap figures.
-    size=2
-    print("Figczar processing: " .. elem.src)
-    -- print(dump(elem))
-    -- print(dump(elem.caption))
-    -- print(pandoc.utils.stringify(elem.caption))
-    if elem.attributes.wrap then
-      local wrappos  
-      if elem.attributes.wrappos then
-        wrappos = elem.attributes.wrappos
-      else
-        wrappos = "R"
-      end
-      local latex_begin=[[\setlength{\intextsep}{2pt}\setlength{\columnsep}{8pt}\begin{wrapfigure}{]] .. wrappos ..[[}{]] .. elem.attributes.wrap .. '}'
-      local latex_fig = [[\centering\includegraphics{]] .. elem.src .. [[}\caption{]]
-      local latex_end = [[}\vspace{0pt}\end{wrapfigure}]]
-      -- print(dump(pandoc.RawInline('latex', latex_begin)))
-      return_value =  {
-        pandoc.RawInline('latex', latex_begin),
-        pandoc.RawInline('latex', latex_fig),
-        pandoc.RawInline('latex', latex_end)
-      }
-      rval = elem.caption
-      table.insert(rval, 1, pandoc.RawInline('latex', latex_fig))
-      table.insert(rval, 1, pandoc.RawInline('latex', latex_begin))
-      table.insert(rval, pandoc.RawInline('latex', latex_end))
-      -- print(dump(rval))
-      -- print(pandoc.utils.stringify(rval))
-      return rval
-    elseif elem.attributes.fullwidth then
-      local latex_begin
-      latex_begin=[[\setlength{\intextsep}{2pt}\setlength{\columnsep}{8pt}\begin{figure*}]] .. '[' .. elem.attributes.fullwidth .. ']'
-      local latex_fig = [[\centering\includegraphics{]] .. elem.src .. [[}\caption{]]
-      local latex_end = [[}\vspace{-5pt}\end{figure*}]]
-      -- print(dump(pandoc.RawInline('latex', latex_begin)))
-      return_value =  {
-        pandoc.RawInline('latex', latex_begin),
-        pandoc.RawInline('latex', latex_fig),
-        pandoc.RawInline('latex', latex_end)
-      }
-      rval = elem.caption
-      table.insert(rval, 1, pandoc.RawInline('latex', latex_fig))
-      table.insert(rval, 1, pandoc.RawInline('latex', latex_begin))
-      table.insert(rval, pandoc.RawInline('latex', latex_end))
-      -- print(dump(rval))
-      -- print(pandoc.utils.stringify(rval))
-      return rval
-    else
-      return elem
-    end
-  end
-end
+  return {
+    -- Pass 1: Figure handler (pandoc 3+)
+    -- Pandoc 3 wraps images in Figure blocks. We must replace the
+    -- entire Figure to avoid nesting figure*/wrapfigure inside figure.
+    {
+      Figure = function(fig)
+        local img = nil
+        fig:walk({
+          Image = function(elem)
+            if elem.attributes.fullwidth or elem.attributes.wrap then
+              img = elem
+            end
+          end
+        })
+        if not img then return nil end
+        print("Figczar processing: " .. img.src)
 
+        -- Use pandoc.write to render caption inlines to LaTeX,
+        -- preserving bold, italics, citations, and other formatting.
+        local caption_latex = pandoc.write(
+          pandoc.Pandoc({pandoc.Plain(fig.caption.long[1].content)}), 'latex'
+        ):gsub('%s+$', '')
+
+        if img.attributes.fullwidth then
+          local latex = [[\setlength{\intextsep}{2pt}\setlength{\columnsep}{8pt}\begin{figure*}]]
+            .. '[' .. img.attributes.fullwidth .. ']'
+            .. [[\centering\includegraphics{]] .. img.src .. [[}]]
+            .. [[\caption{]] .. caption_latex .. [[}]]
+            .. [[\vspace{-5pt}\end{figure*}]]
+          return pandoc.RawBlock('latex', latex)
+
+        elseif img.attributes.wrap then
+          local wrappos = img.attributes.wrappos or "R"
+          local latex = [[\setlength{\intextsep}{2pt}\setlength{\columnsep}{8pt}\begin{wrapfigure}{]]
+            .. wrappos .. [[}{]] .. img.attributes.wrap .. '}'
+            .. [[\centering\includegraphics{]] .. img.src .. [[}]]
+            .. [[\caption{]] .. caption_latex .. [[}]]
+            .. [[\vspace{0pt}\end{wrapfigure}]]
+          return pandoc.RawBlock('latex', latex)
+        end
+      end
+    },
+
+    -- Pass 2: Image handler (pandoc 2 fallback)
+    -- In pandoc 2, there is no Figure element; images are bare Para [Image ...].
+    -- This pass only fires for images that were NOT already handled by pass 1.
+    {
+      Image = function(elem)
+        print("Figczar processing: " .. elem.src)
+        if elem.attributes.wrap then
+          local wrappos
+          if elem.attributes.wrappos then
+            wrappos = elem.attributes.wrappos
+          else
+            wrappos = "R"
+          end
+          local latex_begin=[[\setlength{\intextsep}{2pt}\setlength{\columnsep}{8pt}\begin{wrapfigure}{]] .. wrappos ..[[}{]] .. elem.attributes.wrap .. '}'
+          local latex_fig = [[\centering\includegraphics{]] .. elem.src .. [[}\caption{]]
+          local latex_end = [[}\vspace{0pt}\end{wrapfigure}]]
+          rval = elem.caption
+          table.insert(rval, 1, pandoc.RawInline('latex', latex_fig))
+          table.insert(rval, 1, pandoc.RawInline('latex', latex_begin))
+          table.insert(rval, pandoc.RawInline('latex', latex_end))
+          return rval
+        elseif elem.attributes.fullwidth then
+          local latex_begin
+          latex_begin=[[\setlength{\intextsep}{2pt}\setlength{\columnsep}{8pt}\begin{figure*}]] .. '[' .. elem.attributes.fullwidth .. ']'
+          local latex_fig = [[\centering\includegraphics{]] .. elem.src .. [[}\caption{]]
+          local latex_end = [[}\vspace{-5pt}\end{figure*}]]
+          rval = elem.caption
+          table.insert(rval, 1, pandoc.RawInline('latex', latex_fig))
+          table.insert(rval, 1, pandoc.RawInline('latex', latex_begin))
+          table.insert(rval, pandoc.RawInline('latex', latex_end))
+          return rval
+        else
+          return elem
+        end
+      end
+    }
+  }
+end
